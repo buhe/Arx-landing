d80a968e178a394d1fce8b4d09e6bb29983861b7
buhe <bugu1986@126.com>
2023-04-06 12:33:59 +0800

    "Update text and title for Featured item in itemFeatured.js to better reflect the topic being discussed."

--- Components/Featured/itemFuatered.js
+++ Components/Featured/itemFuatered.js
@@ -2,38 +2,32 @@ const itemFeatured = [
   {
     id: 1,
     image: "Diamond.svg",
-    title: "Assumenda totam culpa",
-    text: "Qui ipsa odio provident sit similique fuga doloremque. Repellendus cumque suscipit consequuntur provident et asperiores ut. Et ut omnis nihil harum et eveniet ex nostrum.",
+    title: "Cross platform mac and iOS",
+    text: "Cross-platform refers to the ability of software or hardware to function across multiple platforms or operating systems. In the case of Mac and iOS, this means that software or applications can be developed to work on both the Mac desktop operating system and the iOS mobile operating system.",
   },
   {
     id: 2,
     image: "Fire.svg",
-    title: "quasi culpa repudiandae",
-    text: "Praesentium dolorem maxime nostrum eius blanditiis autem ducimus est. Aut laborum cumque sunt consequatur aut. Quod voluptatum nobis est. Ipsam sit libero tempora fugit sint aut fugit.",
+    title: "Pass GFW default",
+    text: "The Great Firewall of China, also known as GFW, is a censorship and surveillance project implemented by the Chinese government to control and monitor Internet access within the country. It operates by blocking access to certain websites and services that are deemed politically sensitive or a threat to national security. Despite its stated purposes, the GFW has been criticized for limiting freedom of speech and hindering innovation and economic growth. Some people have found ways to bypass the GFW using tools such as virtual private networks (VPNs) or proxy servers, but such methods are not foolproof and may come with legal risks. Ultimately, the decision to pass or circumvent the GFW is up to the individual and their own values and priorities.",
   },
   {
     id: 3,
     image: "Lightning.svg",
-    title: "Repellat excepturi sunt",
-    text: "Vel velit voluptatibus aut exercitationem rerum officia ex laborum totam. Atque architecto qui ullam non ea praesentium necessitatibus velit sit. Sequi ut nesciunt non.",
+    title: "Mac menu bar style",
+    text: "The Mac menu bar style is a distinctive feature of the macOS operating system, characterized by a horizontal bar located at the top of the screen that displays a series of drop-down menus with various options and commands. This design allows for quick and easy access to system settings, application preferences, and other functions without cluttering the desktop or requiring users to navigate through multiple windows. Additionally, the menu bar is customizable, allowing users to add or remove icons and rearrange the order of menus according to their preferences. Overall, the Mac menu bar style is a key element of macOS's user interface, providing a streamlined and intuitive way to interact with the operating system and applications.",
   },
   {
     id: 4,
     image: "Layout.svg",
-    title: "Modi et earum",
-    text: "At error sequi minima modi est quaerat. Aliquid dolor et. Vel tenetur voluptatum. Quod minima nobis et enim quam et incidunt. Rerum omnis iure omnis.",
+    title: "Prompt management",
+    text: "is essential in any organization or business, as it ensures that tasks are completed efficiently and effectively. Effective prompt management involves setting realistic goals, prioritizing tasks, and providing clear and concise communication to the team. It also involves anticipating potential issues and proactively taking measures to prevent them from occurring. By implementing prompt management strategies, businesses can ensure that their team is working towards achieving their goals and delivering high-quality results in a timely manner. Proper prompt management can also lead to increased productivity, improved morale, and overall success for the organization.",
   },
   {
     id: 5,
     image: "Settings.svg",
-    title: "Earum porro qui",
-    text: "Vel officia dolores ut iusto mollitia velit quis. Ex repudiandae nemo. Omnis ut fuga recusandae. Natus eos inventore. Nesciunt quo asperiores ut quas non molestias vero. Autem ab ducimus.",
-  },
-  {
-    id: 6,
-    image: "Power-button.svg",
-    title: "Et placeat amet",
-    text: "Sint et nihil blanditiis dolorem quod. Quo nostrum cum sequi non consequatur dolores sit. Dolorum optio minus voluptatem dolores et placeat voluptatem necessitatibus est.",
+    title: "Global key: option + e",
+    text: "The global key combination of option + e can be used for various functions depending on the software being used. In Microsoft Word, it can be used to add an acute accent to a letter, while in Adobe Photoshop, it can be used to bring up the Levels adjustment window. It is a versatile tool that can be customized in some software to perform specific actions that can increase efficiency and productivity. Whether it is used for text formatting or photo editing, the option + e combination is a valuable tool for many professionals.",
   },
 ];
 
